914e149c79bd5e958fa2e7393390b8b2a28e5f5e
safeer2978 <safeerkhan2978@gmail.com>
2020-02-01 21:56:48 +0530

    Added Activities and Fragments

---
+++ app/src/main/java/com/treasurehunt/RoundEndActivity.java
@@ -0,0 +1,13 @@
+package com.treasurehunt;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RoundEndActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_round_end);
+    }
+}
---
+++ app/src/main/java/com/treasurehunt/RoundProgressActivity.java
@@ -0,0 +1,13 @@
+package com.treasurehunt;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RoundProgressActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_progress);
+    }
+}
