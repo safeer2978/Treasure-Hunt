57f9c8075bcff49f6b3d98f21465be5b8b03390c
safeer2978 <safeerkhan2978@gmail.com>
2020-02-03 18:49:43 +0530

    added res

--- app/src/main/java/com/treasurehunt/LoginActivity.java
+++ app/src/main/java/com/treasurehunt/LoginActivity.java
@@ -2,12 +2,19 @@ package com.treasurehunt;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
 
+    Button button;
+    EditText etext1,etext2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        button = findViewById(R.id.button);
+        etext1 = findViewById();
+        etext2 = findViewById();
     }
 }
