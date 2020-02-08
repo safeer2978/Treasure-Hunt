d8e47651a2b8c040f1e72f358c737a4d0647df34
Prakhar212001 <55311562+Prakhar212001@users.noreply.github.com>
2020-02-08 15:37:54 +0530

    hjhj

--- app/src/main/java/com/treasurehunt/RoundEndActivity.java
+++ app/src/main/java/com/treasurehunt/RoundEndActivity.java
@@ -1,14 +1,22 @@
 package com.treasurehunt;
 
+import android.content.Intent;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class RoundEndActivity extends AppCompatActivity {
 
@@ -28,11 +36,57 @@ public class RoundEndActivity extends AppCompatActivity {
 //        requestQueue.add(resultRequest);
     }
 
-    public void onClickFeedBack(View v){
+    public void onClickFeedBack(View v) {
+
+        StringRequest resultRequest = new StringRequest(Request.Method.POST, login_url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject res = new JSONObject(response);
+                    //TODO Login result in JSON
+                    int status = (int) res.get("status");
+                    if (status > 0) {
+                        int pos = res.getInt("position");
+                        position.setText(pos);
+                    }
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    position.setText("ERR");
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                position.setText("ERR");
+            }
+        }){};
 
         requestQueue.add(feedbackRequest);
-    }
 
-    StringRequest resultRequest;
-    StringRequest feedbackRequest;
-}
+
+        StringRequest feedbackRequest = new StringRequest(Request.Method.POST, login_url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject res = new JSONObject(response);
+                    //TODO Login result in JSON
+                    int status = (int) res.get("status");
+                    if (status > 0) {
+
+                    }
+                    }
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+    }
